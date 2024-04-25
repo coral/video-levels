02af36d77296381db49ccb23044352dbac5f0ce3
coral <jonas@jonasbengtson.se>
2024-04-24 21:03:25 -0700

    woof

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 If you've had to configure encoders of various kinds, you've probably stumbled upon [level tables](https://en.wikipedia.org/wiki/High_Efficiency_Video_Coding_tiers_and_levels). These are fun until you realize you have to copy this stuff into your code manually or "JUST SET LEVEL 5.2 BRO" which... sadly is more true than you think. Since I didn't want to to that I ended up writing this small helper tool that just has all this in code with sane checking so you can find the right level at runtime.
 
 ```rust
+use video_levels::hevc::{LevelSelector, Profile, Tier};
+
 let level = LevelSelector::new()
     .width(1920)
     .height(1080)
@@ -14,9 +16,11 @@ let level = LevelSelector::new()
 println!("Level: {:?}", level.unwrap().id());
 ```
 
-But hey, your fancy MEDIATEK garbage dump android phone hardware decodeder doesn't support every level in the world? The TL;DR of what you want to do is clamp up to the level required by your configuration (width,height,fps etc) and then clamp up to a level that allows you headroom for your application. This is as easy as:
+But hey, your fancy MediaTek garbage dump android phone hardware decodeder doesn't support every level in the world? The TL;DR of what you want to do is clamp up to the level required by your configuration (width,height,fps etc) and then clamp up to a level that allows you headroom for your application. This is as easy as:
 
 ```rust
+use video_levels::hevc::{LevelSelector, Profile, Tier};
+
 let level = LevelSelector::new()
     .width(3840)
     .height(2160)
@@ -36,11 +40,10 @@ after going insane reading wikipedia and the ITU specs I was like... yeah ok i d
 
 ## Progress
 
-Implemented:
-
 - [x] HEVC
 - [x] AV1
 - [ ] H264 (Dreading this one lol)
+- [ ] Make selector a trait
 
 ## Contributing
 
--- src/av1.rs
+++ src/av1.rs
@@ -4,6 +4,107 @@ use crate::common::ProfileConstraint;
 use yuv::color::ChromaSampling;
 use yuv::color::Depth;
 
+#[derive(Debug)]
+pub struct LevelSelector {
+    // Constraints
+    width: u32,
+    height: u32,
+    framerate: f32,
+    tier: Tier,
+    profile: Profile,
+    min_level: Option<Level>,
+    max_level: Option<Level>,
+    max_bitrate: Option<u32>,
+}
+
+impl LevelSelector {
+    pub fn new() -> Self {
+        Self {
+            // Define default behaviour if no constraints are set
+            width: 1920,
+            height: 1080,
+            framerate: 30.0,
+            tier: Tier::Main,
+            profile: Profile::Main,
+            // Ignore if not set
+            min_level: None,
+            max_level: None,
+            max_bitrate: None,
+        }
+    }
+    pub fn width(mut self, width: u32) -> Self {
+        self.width = width;
+        self
+    }
+    pub fn height(mut self, height: u32) -> Self {
+        self.height = height;
+        self
+    }
+    pub fn framerate(mut self, framerate: f32) -> Self {
+        self.framerate = framerate;
+        self
+    }
+    pub fn clamp(mut self, min: Level, max: Level) -> Self {
+        self.min_level = Some(min);
+        self.max_level = Some(max);
+        self
+    }
+    pub fn max_bitrate(mut self, max_bitrate: u32) -> Self {
+        self.max_bitrate = Some(max_bitrate);
+        self
+    }
+    pub fn tier(mut self, tier: Tier) -> Self {
+        self.tier = tier;
+        self
+    }
+    pub fn profile(mut self, profile: Profile) -> Self {
+        self.profile = profile;
+        self
+    }
+
+    pub fn select(self) -> Option<LevelSpecification> {
+        let samples = self.width * self.height;
+        let display_rate = (samples as f64 * self.framerate as f64) as u64;
+
+        for level in LEVEL_DETAILS.iter() {
+            if samples as u64 <= level.max_picture_size()
+                && display_rate <= level.max_display_rate()
+                && self.width <= level.max_width()
+                && self.height <= level.max_height()
+                && self.framerate as u32 <= level.max_header_rate()
+            {
+                let selected = match (self.max_bitrate, level.max_bit_rate(self.tier)) {
+                    (Some(bitrate_constraint), Some(level_max_bitrate))
+                        if level_max_bitrate >= bitrate_constraint.into() =>
+                    {
+                        *level
+                    }
+                    (None, Some(_)) => *level,
+                    _ => continue,
+                };
+
+                // Clamp to min level
+                match self.min_level {
+                    Some(min) if selected.id() < min => {
+                        continue;
+                    }
+                    _ => {}
+                }
+
+                // Check if exceds max level
+                match self.max_level {
+                    Some(max) if selected.id() > max => return None,
+                    _ => {}
+                }
+
+                return Some(selected);
+            }
+        }
+
+        Some(LEVEL_DETAILS[LEVEL_DETAILS.len() - 1])
+    }
+}
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 pub enum Profile {
     Main,
@@ -150,8 +251,8 @@ pub fn get(level: Level) -> LevelSpecification {
 pub struct LevelSpecification {
     id: Level,
     max_picture_size: u64,
-    max_height: u32,
-    max_width: u32,
+    max_horizontal: u32,
+    max_vertical: u32,
     max_display_rate: u64,
     max_decode_rate: u64,
     max_header_rate: u32,
@@ -169,11 +270,17 @@ impl LevelSpecification {
     pub fn max_picture_size(&self) -> u64 {
         self.max_picture_size
     }
-    pub fn max_height(&self) -> u32 {
-        self.max_height
+    pub fn max_horizontal(&self) -> u32 {
+        self.max_horizontal
+    }
+    pub fn max_vertical(&self) -> u32 {
+        self.max_vertical
     }
     pub fn max_width(&self) -> u32 {
-        self.max_width
+        self.max_horizontal
+    }
+    pub fn max_height(&self) -> u32 {
+        self.max_vertical
     }
     pub fn max_display_rate(&self) -> u64 {
         self.max_display_rate
@@ -211,8 +318,8 @@ pub const LEVEL_DETAILS: [LevelSpecification; 14] = [
     LevelSpecification {
         id: Level::L2,
         max_picture_size: 147456,
-        max_height: 1152,
-        max_width: 2048,
+        max_horizontal: 1152,
+        max_vertical: 2048,
         max_display_rate: 4423680,
         max_decode_rate: 5529600,
         max_header_rate: 150,
@@ -225,8 +332,8 @@ pub const LEVEL_DETAILS: [LevelSpecification; 14] = [
     LevelSpecification {
         id: Level::L2_1,
         max_picture_size: 278784,
-        max_height: 1584,
-        max_width: 2816,
+        max_horizontal: 1584,
+        max_vertical: 2816,
         max_display_rate: 8363520,
         max_decode_rate: 10454400,
         max_header_rate: 150,
@@ -239,8 +346,8 @@ pub const LEVEL_DETAILS: [LevelSpecification; 14] = [
     LevelSpecification {
         id: Level::L3,
         max_picture_size: 665856,
-        max_height: 2448,
-        max_width: 4352,
+        max_horizontal: 2448,
+        max_vertical: 4352,
         max_display_rate: 19975680,
         max_decode_rate: 24969600,
         max_header_rate: 150,
@@ -253,8 +360,8 @@ pub const LEVEL_DETAILS: [LevelSpecification; 14] = [
     LevelSpecification {
         id: Level::L3_1,
         max_picture_size: 1065024,
-        max_height: 3096,
-        max_width: 5504,
+        max_horizontal: 3096,
+        max_vertical: 5504,
         max_display_rate: 31950720,
         max_decode_rate: 39938400,
         max_header_rate: 150,
@@ -267,8 +374,8 @@ pub const LEVEL_DETAILS: [LevelSpecification; 14] = [
     LevelSpecification {
         id: Level::L4,
         max_picture_size: 2359296,
-        max_height: 3456,
-        max_width: 6144,
+        max_horizontal: 3456,
+        max_vertical: 6144,
         max_display_rate: 70778880,
         max_decode_rate: 77856768,
         max_header_rate: 300,
@@ -281,8 +388,8 @@ pub const LEVEL_DETAILS: [LevelSpecification; 14] = [
     LevelSpecification {
         id: Level::L4_1,
         max_picture_size: 2359296,
-        max_height: 3456,
-        max_width: 6144,
+        max_horizontal: 3456,
+        max_vertical: 6144,
         max_display_rate: 141557760,
         max_decode_rate: 155713536,
         max_header_rate: 300,
@@ -295,8 +402,8 @@ pub const LEVEL_DETAILS: [LevelSpecification; 14] = [
     LevelSpecification {
         id: Level::L5,
         max_picture_size: 8912896,
-        max_height: 4352,
-        max_width: 8192,
+        max_horizontal: 4352,
+        max_vertical: 8192,
         max_display_rate: 267386880,
         max_decode_rate: 273715200,
         max_header_rate: 300,
@@ -309,8 +416,8 @@ pub const LEVEL_DETAILS: [LevelSpecification; 14] = [
     LevelSpecification {
         id: Level::L5_1,
         max_picture_size: 8912896,
-        max_height: 4352,
-        max_width: 8192,
+        max_horizontal: 4352,
+        max_vertical: 8192,
         max_display_rate: 534773760,
         max_decode_rate: 547430400,
         max_header_rate: 300,
@@ -323,8 +430,8 @@ pub const LEVEL_DETAILS: [LevelSpecification; 14] = [
     LevelSpecification {
         id: Level::L5_2,
         max_picture_size: 8912896,
-        max_height: 4352,
-        max_width: 8192,
+        max_horizontal: 4352,
+        max_vertical: 8192,
         max_display_rate: 1069547520,
         max_decode_rate: 1094860800,
         max_header_rate: 300,
@@ -337,8 +444,8 @@ pub const LEVEL_DETAILS: [LevelSpecification; 14] = [
     LevelSpecification {
         id: Level::L5_3,
         max_picture_size: 8912896,
-        max_height: 4352,
-        max_width: 8192,
+        max_horizontal: 4352,
+        max_vertical: 8192,
         max_display_rate: 1069547520,
         max_decode_rate: 1176502272,
         max_header_rate: 300,
@@ -351,8 +458,8 @@ pub const LEVEL_DETAILS: [LevelSpecification; 14] = [
     LevelSpecification {
         id: Level::L6,
         max_picture_size: 35651584,
-        max_height: 8704,
-        max_width: 16384,
+        max_horizontal: 8704,
+        max_vertical: 16384,
         max_display_rate: 1069547520,
         max_decode_rate: 1176502272,
         max_header_rate: 300,
@@ -365,8 +472,8 @@ pub const LEVEL_DETAILS: [LevelSpecification; 14] = [
     LevelSpecification {
         id: Level::L6_1,
         max_picture_size: 35651584,
-        max_height: 8704,
-        max_width: 16384,
+        max_horizontal: 8704,
+        max_vertical: 16384,
         max_display_rate: 2139095040,
         max_decode_rate: 2189721600,
         max_header_rate: 300,
@@ -379,8 +486,8 @@ pub const LEVEL_DETAILS: [LevelSpecification; 14] = [
     LevelSpecification {
         id: Level::L6_2,
         max_picture_size: 35651584,
-        max_height: 8704,
-        max_width: 16384,
+        max_horizontal: 8704,
+        max_vertical: 16384,
         max_display_rate: 4278190080,
         max_decode_rate: 4379443200,
         max_header_rate: 300,
@@ -393,8 +500,8 @@ pub const LEVEL_DETAILS: [LevelSpecification; 14] = [
     LevelSpecification {
         id: Level::L6_3,
         max_picture_size: 35651584,
-        max_height: 8704,
-        max_width: 16384,
+        max_horizontal: 8704,
+        max_vertical: 16384,
         max_display_rate: 4278190080,
         max_decode_rate: 4706009088,
         max_header_rate: 300,
@@ -424,5 +531,48 @@ mod tests {
         let l = av1::get(Level::L5_2);
         assert_eq!(l.max_bit_rate(Tier::Main), Some(60_000_000));
         assert_eq!(l.max_bit_rate(Tier::High), Some(240_000_000));
+    }
+
+    #[test]
+    fn select_base_cases() {
+        use crate::av1::{Level, LevelSelector, Profile, Tier};
+        assert_eq!(
+            LevelSelector::new()
+                .width(1920)
+                .height(1080)
+                .framerate(30.0)
+                .tier(Tier::Main)
+                .profile(Profile::Main)
+                .select()
+                .unwrap()
+                .id(),
+            Level::L4
+        );
+
+        assert_eq!(
+            LevelSelector::new()
+                .width(4380)
+                .height(1080)
+                .framerate(30.0)
+                .tier(Tier::Main)
+                .profile(Profile::Main)
+                .select()
+                .unwrap()
+                .id(),
+            Level::L6
+        );
+
+        assert_eq!(
+            LevelSelector::new()
+                .width(3840)
+                .height(2160)
+                .framerate(200.0)
+                .tier(Tier::Main)
+                .profile(Profile::Main)
+                .select()
+                .unwrap()
+                .id(),
+            Level::L6_1
+        );
     }
 }
